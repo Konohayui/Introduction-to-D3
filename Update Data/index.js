719fca54d4e6934fe06dee2f32f566f82cc54c21
shenmemingzine <rongwingyu@gmail.com>
2019-06-06 14:26:42 -0700

    Update index.js

--- Update Data/index.js
+++ Update Data/index.js
@@ -4,56 +4,56 @@ var svgWidth = 500, svgHeight = 300, margin = 30;
 
 function plot(data) {
 	var svg = d3.selectAll("svg")
-						.attr("width", svgWidth + 2*margin)
-            .attr("height", svgHeight + 2*margin)
-            .append("g")
-            .attr("transform", "translate(" + margin + "," + margin + ")");
+		    .attr("width", svgWidth + 2*margin)
+                    .attr("height", svgHeight + 2*margin)
+                    .append("g")
+                    .attr("transform", "translate(" + margin + "," + margin + ")");
             
-	var barWidth = svgWidth/data.length;
+  	var barWidth = svgWidth/data.length;
   
-  var xScale = d3.scaleLinear()
-  							 .domain([0, data.length])
-                 .range([0, svgWidth]);
+ 	 var xScale = d3.scaleLinear()
+  			 .domain([0, data.length])
+  	               .range([0, svgWidth]);
                  
-  var yScale = d3.scaleLinear()
-  							 .domain([0, d3.max(data)])
-                 .range([svgHeight, 0]);
+ 	 var yScale = d3.scaleLinear()
+  			.domain([0, d3.max(data)])
+                 	.range([svgHeight, 0]);
                  
                  
-  var xaxis = d3.axisBottom()
-  						  .scale(xScale);
+  	var xaxis = d3.axisBottom()
+		      .scale(xScale);
   
-  var yaxis = d3.axisLeft()
-  							.scale(yScale);
+  	var yaxis = d3.axisLeft()
+  		      .scale(yScale);
                 
-  var barChart = svg.selectAll("rect")
-  									.data(data)
-                    .enter()
-                    .append("rect")
-                    .attr("fill", "lightskyblue")
-                    .attr("y", function(d) { return yScale(d); })
-                    .attr("height", function(d) { return svgHeight - yScale(d); })
-                    .attr("width", barWidth)
-                    .attr("transform", function(d, i) {
-                    	var translate = [barWidth*i, 0];
-                      return "translate(" + translate + ")";
-                    });
+  	var barChart = svg.selectAll("rect")
+  			  .data(data)
+                    	  .enter()
+                          .append("rect")
+                          .attr("fill", "lightskyblue")
+                          .attr("y", function(d) { return yScale(d); })
+                          .attr("height", function(d) { return svgHeight - yScale(d); })
+                          .attr("width", barWidth)
+                          .attr("transform", function(d, i) {
+                    					var translate = [barWidth*i, 0];
+                      					return "translate(" + translate + ")";
+                    	   });
                     
-  var text = svg.selectAll("text")
-  							.data(data)
-                .enter()
-                .append("text")
-                .text(function(d) { return d; })
-                .attr("y", function(d) { return yScale(d) - 2; })
-                .attr("x", function(d, i) { return barWidth*i + barWidth/2 - 10; })
-                .attr("fill", "#000000");
+  	var text = svg.selectAll("text")
+  		      .data(data)
+                      .enter()
+                      .append("text")
+                      .text(function(d) { return d; })
+                      .attr("y", function(d) { return yScale(d) - 2; })
+                      .attr("x", function(d, i) { return barWidth*i + barWidth/2 - 10; })
+                      .attr("fill", "#000000");
                 
-  svg.append("g")
-  	 .attr("transform", "translate(0," + svgHeight + ")")
-  	 .call(xaxis);
+       svg.append("g")
+  	  .attr("transform", "translate(0," + svgHeight + ")")
+  	  .call(xaxis);
      
-  svg.append("g")
-  	 .call(yaxis);
+       svg.append("g")
+  	  .call(yaxis);
                     
 }
 
